545a4d70fad864cb898bc08301d7e826fd85a293
perpel <572821520@qq.com>
2024-04-29 10:34:53 +0800

    m

--- go.mod
+++ go.mod
@@ -1,11 +1,8 @@
-module github.com/kordar/nazalog
+module github.com/kordar/gologger_nazalog
 
 go 1.16
 
 require (
-	github.com/kordar/nazacolor v0.0.1
-	github.com/kordar/nazafake v0.0.2
-	github.com/kordar/nazamock v0.0.3
-	github.com/kordar/nazareflect v0.0.1
-	github.com/kordar/nazassert v0.0.2
+	github.com/kordar/gologger v0.0.6
+	github.com/q191201771/naza v0.30.48
 )
---
+++ nazalog.go
@@ -0,0 +1,94 @@
+package gologger_nazalog
+
+import (
+	logger "github.com/kordar/gologger"
+	"github.com/q191201771/naza/pkg/nazalog"
+)
+
+type nazalogAdapt struct {
+	l nazalog.Logger
+}
+
+func NewNazalogAdapt() logger.Logger {
+	return nazalogAdapt{l: nazalog.GetGlobalLogger()}
+}
+
+func (n nazalogAdapt) WithField(key string, value interface{}) logger.Logger {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (n nazalogAdapt) WithFields(fields logger.Fields) logger.Logger {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (n nazalogAdapt) Trace(args ...interface{}) {
+	//TODO implement me
+	n.l.Trace(args...)
+}
+
+func (n nazalogAdapt) Tracef(format string, args ...interface{}) {
+	//TODO implement me
+	n.l.Tracef(format, args...)
+}
+
+func (n nazalogAdapt) Debug(args ...interface{}) {
+	//TODO implement me
+	n.l.Debug(args...)
+}
+
+func (n nazalogAdapt) Debugf(format string, args ...interface{}) {
+	//TODO implement me
+	n.l.Debugf(format, args...)
+}
+
+func (n nazalogAdapt) Info(args ...interface{}) {
+	//TODO implement me
+	n.l.Info(args...)
+}
+
+func (n nazalogAdapt) Infof(format string, args ...interface{}) {
+	//TODO implement me
+	n.l.Infof(format, args...)
+}
+
+func (n nazalogAdapt) Warn(args ...interface{}) {
+	//TODO implement me
+	n.l.Warn(args...)
+}
+
+func (n nazalogAdapt) Warnf(format string, args ...interface{}) {
+	//TODO implement me
+	n.l.Warnf(format, args...)
+}
+
+func (n nazalogAdapt) Error(args ...interface{}) {
+	//TODO implement me
+	n.l.Error(args...)
+}
+
+func (n nazalogAdapt) Errorf(format string, args ...interface{}) {
+	//TODO implement me
+	n.l.Errorf(format, args...)
+}
+
+func (n nazalogAdapt) Panic(args ...interface{}) {
+	//TODO implement me
+	n.l.Panic(args...)
+}
+
+func (n nazalogAdapt) Panicf(format string, args ...interface{}) {
+	//TODO implement me
+	n.l.Panicf(format, args...)
+}
+
+func (n nazalogAdapt) Fatal(args ...interface{}) {
+	//TODO implement me
+	n.l.Fatal(args...)
+}
+
+func (n nazalogAdapt) Fatalf(format string, args ...interface{}) {
+	//TODO implement me
+	n.l.Fatalf(format, args...)
+}
